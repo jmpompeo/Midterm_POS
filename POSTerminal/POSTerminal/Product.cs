f49a62898dbb371f07ecc21e96c21765540c003b
Chike Okoye <iokoye217@gmail.com>
2021-02-22 22:00:12 -0500

    product class

---
+++ POSTerminal/POSTerminal/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSTerminal
+{
+    class Product
+    {
+        public string Name { get; }
+        public string Category { get; }
+        public string Description { get; }
+        public int Price { get; }
+
+
+
+
+
+    }
+}
